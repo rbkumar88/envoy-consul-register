7edf4c44e54b5551c37adc88f2a12b2b3cf76851
Bharath Kumar Ramasubramanian <rbkumar88@gmail.com>
2019-05-16 10:48:21 -0700

    Update types.go

--- controller/types.go
+++ controller/types.go
@@ -1,11 +1,5 @@
 package controller
 
-import (
-	"github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
-	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
-	"github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
-)
-
 // FactoryAdapter has a method to work with Controller resources.
 type ConsulServiceConfig struct {
 	ID    string
@@ -23,8 +17,8 @@ type ConsulServiceConfig struct {
 	ServiceName string
 }
 
-type EnvoyServiceConfig struct {
-	RetryPolicy *route.RetryPolicy `protobuf:"bytes,16,opt,name=retry_policy,json=retryPolicy,proto3" json:"retry_policy,omitempty"`
-	HealthChecks []*core.HealthCheck `protobuf:"bytes,8,rep,name=health_checks,json=healthChecks,proto3" json:"health_checks,omitempty"`
-	TlsContext *auth.UpstreamTlsContext `protobuf:"bytes,11,opt,name=tls_context,json=tlsContext,proto3" json:"tls_context,omitempty"`
-}
+//type EnvoyServiceConfig struct {
+//	RetryPolicy *route.RetryPolicy `protobuf:"bytes,16,opt,name=retry_policy,json=retryPolicy,proto3" json:"retry_policy,omitempty"`
+//	HealthChecks []*core.HealthCheck `protobuf:"bytes,8,rep,name=health_checks,json=healthChecks,proto3" json:"health_checks,omitempty"`
+//	TlsContext *auth.UpstreamTlsContext `protobuf:"bytes,11,opt,name=tls_context,json=tlsContext,proto3" json:"tls_context,omitempty"`
+//}
