3049d9a4f2a4de42bb2f09c115210306ffe7ab35
Bharath Kumar Ramasubramanian <rbkumar88@gmail.com>
2019-05-21 13:59:16 -0700

    Update types.go

--- controller/types.go
+++ controller/types.go
@@ -16,9 +16,3 @@ type ConsulServiceConfig struct {
 	EnvoyDynamicConfig *EnvoyServiceConfig
 	ServiceName string
 }
-
-//type EnvoyServiceConfig struct {
-//	RetryPolicy *route.RetryPolicy `protobuf:"bytes,16,opt,name=retry_policy,json=retryPolicy,proto3" json:"retry_policy,omitempty"`
-//	HealthChecks []*core.HealthCheck `protobuf:"bytes,8,rep,name=health_checks,json=healthChecks,proto3" json:"health_checks,omitempty"`
-//	TlsContext *auth.UpstreamTlsContext `protobuf:"bytes,11,opt,name=tls_context,json=tlsContext,proto3" json:"tls_context,omitempty"`
-//}
